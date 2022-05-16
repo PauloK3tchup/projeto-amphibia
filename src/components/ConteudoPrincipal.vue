179fce7d12c36fd482c8d4528949556133e48529
PauloK3tchup <passandoalimpo13@gmail.com>
2022-05-16 15:21:34 -0300

    Lore Atualizada

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,108 +1,50 @@
-<script>
-import CompOpcao from "./CompOpcao.vue";
-export default {
-  components: { CompOpcao },
-};
-</script>
+<script></script>
 <template>
   <main id="content">
-    <div id="Introdução">
-      <h1 class="comecoP glow">Projeto Amphibia</h1>
-      <h1>Seja Bem-vindo ao meu site!</h1>
-      <h3>
-        Aqui você verá curiosidades e análises do desenho animado Amphibia,
-        feitas de fã para fã!
-      </h3>
-      <img
-        src="../assets/img/Fundo_Casa-dos-Plantas.jpg"
-        alt="Casa dos Plantars"
-      />
-      <h1>Atenção!!!</h1>
-      <h3>
-        Esse site está entupido de spoilers, se você não assistiu e gostaria de
-        ter uma experiência sem spoilers eu recomendo que você saia deste site
-        AGORA! (A não ser que você não se importe com spoilers, embora eu
-        recomende que você tenha a experiência completa)
-      </h3>
-      <h3>
-        Esse site foi produzido como um trabalho escolar para o segundo ano do
-        ensino médio (vou colocar a nota aqui dps que entregar kk) mas também
-        como uma forma de compartilhar os aspectos que eu mais gosto desse
-        desenho com as pessoas que quiserem acessá-lo, então prepare-se pois
-        vamos entrar em uma aventura e tanto!!!11!!1!!!
-      </h3>
-    </div>
-    <div id="Resumo">
-      <h1>Sobre o que é o desenho?</h1>
-      <h3>
-        O desenho é sobre uma garota tailandesa chamada Anne Boochuy que abriu
-        uma caixa de músicas misteriosa e foi parar em outro mundo chamado
-        Amphibia. Nesse mundo ela foi acolhida pela família Plantar da cidade de
-        Wartwood e decide ficar lá até descobrir como reativar a caixa de música
-        e retornar para o planeta terra.
-      </h3>
-      <img src="../assets/img/Abertura.gif" alt="Primeira cena da abertura" />
-      <h3>
-        O desenho estreou em 17 de julho de 2019 noos EUA e em 5 de agosto de
-        2019 no Brasil e conta com 3 temporadas e 58 episódios tendo deu último
-        episódio lançado em 14 de maio de 2022. O desenho também possui uma nota
-        geral de 8,1/10 no IMDB e 9,8/10 em seu melhor episódio. <br />
-        O desenho foi criado por Matt Braly, um animador, diretor, escritor,
-        artista de storyboard e produtor americano conhecido anteriormente por
-        ter trabalhado como diretor em Gravity Falls.
-      </h3>
-      <h3>
-        O desenho teve seu percurso na Disney ao lado de outro desenho chamado
-        The Owl House, que embora tenha estreado no ano seguinte, acompanhou
-        Amphibia do início ao fim de seu percurso, fazendo com que muitas das
-        pessoas que assistissem Amphibia também acabassem assistindo The Owl
-        House (principalmente quase se considera a amizade dos criadores de
-        ambos desenhos e o quanto eles interagem online).
-      </h3>
-      <h3>
-        Apesar do foco em comédia, o desenho possui uma tonelada de segredos e
-        detalhes ao longo que tornam a lore do mesmo muito mais complexa, desde
-        de um sistema político corrupto até profecias e horrores desumanos além
-        de nossa compreensão.
-      </h3>
-      <h2>
-        Abaixo você poderá escolher qual aspecto do desenho deseja explorar
-      </h2>
-      <img src="../assets/img/Caixa-De-Música.jpg" alt="Caixa da calamidade" />
-      <h1 class="glow">Para onde deseja ir?</h1>
-    </div>
-    <div class="container">
-      <div id="OutrasOpções">
-        <CompOpcao class="Lore glow" texto="Lore" />
-        <CompOpcao class="Episódios glow" texto="Episodios" />
-        <CompOpcao class="Segredos glow" texto="Segredos" />
-        <CompOpcao class="Personagens glow" texto="Personagens" />
-      </div>
-    </div>
-
-    <br /><br />
-
-    <div id="Explicação">
-      <h1>
-        Cada um dos botões acima vai te levar para as informações que deseja
-      </h1>
-      <h3>
-        O botão Lore vai te levar para a página com um resumo da lore do
-        desenho, desde o início de Newtopia até o fim do desenho.
-      </h3>
-      <h3>
-        O botão Episódios vai te levar para a página com uma lista de todos os
-        episódios do desenho.
-      </h3>
-      <h3 class="codf">
-        O botao Segredos vai te levar para uma pagina com os segredos e runas de
-        Amphibia.
-      </h3>
-      <h3>
-        O botão Personagens vai te levar para uma página com um resumo dos
-        pesonagens mais importantes.
+    <h1 class="comecoP glow">Lore</h1>
+    <h1 class="codificado">Three stars burning bright</h1>
+    <img
+      src="../assets/img/DarcyFoice.gif"
+      alt="Darcy lutando contra Sasha e Grime"
+      class="grande"
+    />
+    <div class="origem">
+      <h1>Vamos começar pelo começo de tudo (literalmente)</h1>
+      <h3 class="justf">
+        Milhares de anos atrás, uma entidade superior criou 3 pedras com poderes
+        ilimitados para ver como os mortais lidam com esse tipo de poder, tudo
+        isso com o objetivo de encontrar algúem digno o suficiente para assumir
+        seu papel de guardião de todos os mundos.
+        <img
+          src="../assets/img/Andrias e Barrel.jpg"
+          alt="Andrias e Barrel"
+          class="Foto-lado"
+        />
+        Ainda é um mistério se essas pedras caíram em algum lugar antes de
+        Amphibia, mas sabemos que os ancestrais de Amphibia utilizaram as gemas
+        para viajar entre mundos através de uma caixa de música, entretando, com
+        o intuito de conquista.
+        <br />
+        As pedras foram consideradadas como "O maior tesouro de Amphibia" por
+        milhares de anos, até que um incidente ocorreu. O então príncipe de
+        Newtopia, Andrias Leviathan, estava prestes a se tornar rei e,
+        consequentemente, tomar controle da caixa de música. Entretando, na
+        comemoração, Leif (Melhor amiga de Andrias ao lado do sapo Barrel)
+        encostou nas gemas e teve uma visão que mostrava a destruição de
+        Amphibia. Leif deduziu que isso significava que a caixa iria destruir
+        Amphibia se continuasse sendo usada e fugiu com ela, traindo Andrias e
+        Barrel. Andrias, revoltado, enviou Barrel para defender algumas vilas do
+        lado de fora do reino, o que levou Barrel a sua morte defendendo uma das
+        vilas.
+        <br />
+        O ódio de Andrias por Leif fez com que ele criasse um sistema de
+        hierarquia no reino que consiste de: <br />
+        Tritões (ou salamandras, geralmente são pessoas de classe ou
+        professores) no topo; <br />
+        Sapos (geralmente como prefeitos ou guerreiros) no meio; <br />
+        E rãs (geralmente fazendeiros) na classe baixa.
       </h3>
     </div>
-    <h1 class="comecoP glow codf">Divirta-se!</h1>
   </main>
 </template>
+<style></style>
---
+++ src/components/ConteudoPrincipalP.vue
@@ -0,0 +1,12 @@
+<script></script>
+<template>
+  <main id="content">
+    <h1 class="comecoP glow">Personagens</h1>
+    <h1 class="codificado">Their choice will determine the fate of all</h1>
+    <img
+      src="../assets/img/TresEstrelas.gif"
+      alt="Anne, Marcy e Sasha com seus poderes"
+    />
+  </main>
+</template>
+<style></style>
